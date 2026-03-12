1799fb70274447abb149883f762d6d793a9458ca
Luminous | 418 <jgutalg2006@outlook.es>
2026-03-12 19:02:25 +0100

    New A34 base

--- scripts/LumiROM.sh
+++ scripts/LumiROM.sh
@@ -37,8 +37,8 @@ DOWNLOAD_FIRMWARE() {
 
     mkdir -p "$DOWN_DIR" || return 1
     
-    echo "Downloading A34 images - 4.88GB"
-    gdown 1AEvZGN3QRv_OrkaiBfi_7BZ_Kx64Pk5q -O "${DOWN_DIR}/SM-A346E_OneUi85_firmware.zip"
+    echo "Downloading A34 images"
+    gdown 13H5W6rptuTsqfidMivuibezLv_5nBuHu -O "${DOWN_DIR}/SM-A346E_OneUi85_firmware.zip"
 
     echo "Downloading vendor for ${STOCK_DEVICE} - 700-800MB"
     wget -q "https://github.com/Luminous418/VendorsForMTKG80/releases/download/${STOCK_DEVICE}_latest/vendor.img" -O "${DOWN_DIR}/vendor.img"
