7e4996a30aab1f0c37fcce048efcc83a9ab162ba
Luminous | 418 <jgutalg2006@outlook.es>
2026-03-16 19:55:38 +0100

    Make it more clear

--- LumiROM_local.sh
+++ LumiROM_local.sh
@@ -48,39 +48,59 @@ export BUILD_PARTITIONS="product,vendor,odm,system_ext,system"
 source "$(pwd)/scripts/LumiROM.sh"
 source "$DEVICES_DIR/$STOCK_DEVICE/config"
 
+# Download firmware
 DOWNLOAD_FIRMWARE "$TARGET_DEVICE" "$FIRM_DIR"
 
+# Extract firmware
 EXTRACT_FIRMWARE "$FIRM_DIR/$TARGET_DEVICE"
 PREPARE_PARTITIONS "$FIRM_DIR/$TARGET_DEVICE"
 EXTRACT_FIRMWARE_IMG "$FIRM_DIR/$TARGET_DEVICE"
 
+# Apply vendor patches
 DISABLE_FBE "$FIRM_DIR/$TARGET_DEVICE"
 DISABLE_FDE "$FIRM_DIR/$TARGET_DEVICE"
 DELETE_ICCC "$FIRM_DIR/$TARGET_DEVICE"
 PATCH_FSTAB_EROFS "$FIRM_DIR/$TARGET_DEVICE"
 
+# Apply stock config
 APPLY_STOCK_CONFIG "$FIRM_DIR/$TARGET_DEVICE"
 
+# Debloat ROM
 DEBLOAT "$FIRM_DIR/$TARGET_DEVICE"
+
+# Apply features
 APPLY_FEATURES "$FIRM_DIR/$TARGET_DEVICE"
 
+# Update Build ID
+APPENDING_DISPLAY_ID "$FIRM_DIR/$TARGET_DEVICE"
+
+# Install stock framework-res
 INSTALL_FRAMEWORK "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/framework-res.apk"
 
+# Decompile framework jars
 DECOMPILE "$APKTOOL" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/ssrm.jar" "$WORK_DIR"
 DECOMPILE "$APKTOOL" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/services.jar" "$WORK_DIR"
 
+# Patch framework
 PATCH_SSRM "$WORK_DIR/ssrm"
 PATCH_KNOX_GUARD "$WORK_DIR/services"
 PATCH_FLAG_SECURE "$WORK_DIR/services"
 PATCH_SECURE_FOLDER "$WORK_DIR/services"
 
+# Recompile framework
 RECOMPILE "$APKTOOL" "$WORK_DIR/ssrm" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework" "$WORK_DIR"
 RECOMPILE "$APKTOOL" "$WORK_DIR/services" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework" "$WORK_DIR"
 cp -fv "$WORK_DIR"/*.jar "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/"
 
+# Build ROM
 BUILD_IMG "$FIRM_DIR/$TARGET_DEVICE" "$OUTPUT_FILESYSTEM" "$OUT_DIR"
 IMG_TO_SDAT_AND_COMPRESS "$OUT_DIR" "TMP"
 
-source scripts/zip_creation.sh
+
+source "$(pwd)/scripts/zip_creation.sh"
+
+# Update updater-script
 UPDATE_ZIP_SCRIPT "$FIRM_DIR/$TARGET_DEVICE"
+
+# Prepare flashable zip
 FLASHABLE_ZIP_CREATION
