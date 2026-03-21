a1b178b882af176480c4827872156451df6553cd
H3CKER <149829807+H33CKER@users.noreply.github.com>
2026-03-21 09:59:36 +0000

    change a226b to a34 base

--- scripts/LumiROM.sh
+++ scripts/LumiROM.sh
@@ -36,9 +36,9 @@ DOWNLOAD_FIRMWARE() {
     
     echo "Downloading ROM images for $STOCK_DEVICE"
 
-        if [[ "$STOCK_DEVICE" == "SM-A325F" || "$STOCK_DEVICE" == "SM-A325M" || "$STOCK_DEVICE" == "SM-M325F" ]]; then
+        if [[ "$STOCK_DEVICE" == "SM-A325F" || "$STOCK_DEVICE" == "SM-A325M" || "$STOCK_DEVICE" == "SM-M325F" || "$STOCK_DEVICE" == "SM-A226B" ]]; then
             gdown 1GttWVXgAYnCg9f2_facbfFbjwdh-KFp8 -O "${DOWN_DIR}/SM-A346E_OneUi85_firmware.zip"
-        elif [[ "$STOCK_DEVICE" == "SM-A225F" || "$STOCK_DEVICE" == "SM-A225M" || "$STOCK_DEVICE" == "SM-A226B" || "$STOCK_DEVICE" == "SM-E225F" || "$STOCK_DEVICE" == "SM-M225F" ]]; then
+        elif [[ "$STOCK_DEVICE" == "SM-A225F" || "$STOCK_DEVICE" == "SM-A225M" || "$STOCK_DEVICE" == "SM-E225F" || "$STOCK_DEVICE" == "SM-M225F" ]]; then
             gdown 1oTWnFVL_A8qjzmFohjOJqMyjQaYQs-xR -O "${DOWN_DIR}/SM-A155F_OneUi85_firmware.zip"
         else
             $STOCK_DEVICE="unknown"
