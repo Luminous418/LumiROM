916fb956a551c2ed34aae8a9b9a19e7a450db22c
H3CKER <gnanadan2010@outlook.com>
2026-04-01 21:26:58 +0100

    make it better

--- scripts/LumiROM.sh
+++ scripts/LumiROM.sh
@@ -853,9 +853,8 @@ APPLY_STOCK_CONFIG() {
     fi
 
 	# Replace Stock Files.
-	find "$EXTRACTED_FIRM_DIR/system/system/media" -maxdepth 1 -type f \( -iname "*.spi" -o -iname "*.qmg" -o -iname "*.txt" \) -delete
 	rm -rf $EXTRACTED_FIRM_DIR/product/overlay/framework-res*auto_generated_rro_product.apk
-	cp -a "$DEVICES_DIR/$STOCK_DEVICE/Stock/." "$EXTRACTED_FIRM_DIR/"
+    cp -af "$DEVICES_DIR/$STOCK_DEVICE/Stock/." "$EXTRACTED_FIRM_DIR/"
 }
 
 
