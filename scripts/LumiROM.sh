96578f579c4a1ea863f6e5b87cf4661aaf0d152b
Luminous | 418 <jgutalg2006@outlook.es>
2026-04-03 12:41:16 +0200

    Put A226B on another firmware

--- scripts/LumiROM.sh
+++ scripts/LumiROM.sh
@@ -56,9 +56,9 @@ DOWNLOAD_FIRMWARE() {
     
     echo "Downloading ROM images for $STOCK_DEVICE"
 
-    if [[ "$STOCK_DEVICE" == "SM-A325F" || "$STOCK_DEVICE" == "SM-A325M" || "$STOCK_DEVICE" == "SM-M325F" || "$STOCK_DEVICE" == "SM-A226B" ]]; then
+    if [[ "$STOCK_DEVICE" == "SM-A325F" || "$STOCK_DEVICE" == "SM-A325M" || "$STOCK_DEVICE" == "SM-M325F" ]]; then
         wget -O "${DOWN_DIR}/SM-A346E_OneUi85_firmware.zip" "https://${SERVER_DIR}A346B.zip?sign=nSPfUDaOWHgPp9J_w-sb56skCDdlDC6hZIB7tYekoC0=:0"
-    elif [[ "$STOCK_DEVICE" == "SM-A225F" || "$STOCK_DEVICE" == "SM-A225M" || "$STOCK_DEVICE" == "SM-E225F" || "$STOCK_DEVICE" == "SM-M225F" ]]; then
+    elif [[ "$STOCK_DEVICE" == "SM-A225F" || "$STOCK_DEVICE" == "SM-A225M" || "$STOCK_DEVICE" == "SM-E225F" || "$STOCK_DEVICE" == "SM-M225F" || "$STOCK_DEVICE" == "SM-A226B" ]]; then
         wget -O "${DOWN_DIR}/SM-A155F_OneUi85_firmware.zip" "https://${SERVER_DIR}A155F.zip?sign=42KKwi4g2iTpzl9TmNYFWbechxpAqB6Qf-REAIbspI8=:0"
     else
         STOCK_DEVICE="unknown"
