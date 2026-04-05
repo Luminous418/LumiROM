994162b716673a5b10383223b1428a3e8a9cb2cd
Luminous | 418 <jgutalg2006@outlook.es>
2026-04-05 19:58:49 +0200

    Update firmware file paths in LumiROM.sh
    If we gonna make it cached now, no sense to make say a name and be another name

--- scripts/LumiROM.sh
+++ scripts/LumiROM.sh
@@ -60,11 +60,11 @@ DOWNLOAD_FIRMWARE() {
     FW_URL=""
 
     if [[ "$STOCK_DEVICE" == "SM-A325F" || "$STOCK_DEVICE" == "SM-A325M" || "$STOCK_DEVICE" == "SM-M325F" ]]; then
-        FW_FILE="${DOWN_DIR}/SM-A346E_OneUi85_firmware.zip"
+        FW_FILE="${DOWN_DIR}/OneUi85_firmware.zip"
         FW_URL="https://h3cked.qzz.io/d/H3CKED_HDD/LumiROM/Base_FW/A346B.zip?sign=nSPfUDaOWHgPp9J_w-sb56skCDdlDC6hZIB7tYekoC0=:0"
 
     elif [[ "$STOCK_DEVICE" == "SM-A225F" || "$STOCK_DEVICE" == "SM-A225M" || "$STOCK_DEVICE" == "SM-E225F" || "$STOCK_DEVICE" == "SM-M225F" || "$STOCK_DEVICE" == "SM-A226B" ]]; then
-        FW_FILE="${DOWN_DIR}/SM-A245F_OneUi85_firmware.zip"
+        FW_FILE="${DOWN_DIR}/OneUi85_firmware.zip"
         FW_URL="https://h3cked.qzz.io/d/H3CKED_HDD/LumiROM/Base_FW/A245F.zip?sign=GpyvunbcV76xw7beb90jkAdYrmpaiUPNv_8uSf1LJ5Y=:0"
     else
         echo "Unknown device: $STOCK_DEVICE"
