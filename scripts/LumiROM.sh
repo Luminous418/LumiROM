36d455ef8b5013ae81c1aedd11f1e8d29b8d1e1c
Luminous | 418 <jgutalg2006@outlook.es>
2026-04-27 13:21:22 +0200

    Fix typo

--- scripts/LumiROM.sh
+++ scripts/LumiROM.sh
@@ -172,7 +172,7 @@ EXTRACT_FIRMWARE_IMG() {
 
 	local FIRM_DIR="$1"
 
-	echo "Extracting imges from $FIRM_DIR"
+	echo "Extracting images from $FIRM_DIR"
     for imgfile in "$FIRM_DIR"/*.img; do
         [ -e "$imgfile" ] || continue
 
