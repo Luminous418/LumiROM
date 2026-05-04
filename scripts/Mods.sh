f5b52234c1f0a99b7ee45fb518879f82234af37e
Luminous | 418 <jgutalg2006@outlook.es>
2026-05-04 18:20:42 +0200

    Try fix for cam

--- scripts/Mods.sh
+++ scripts/Mods.sh
@@ -19,6 +19,7 @@ ADD_MODS() {
         sudo cp -rfa "$(pwd)/LumiROM/Mods/volte_fix/vendor/lib64/"* "$EXTRACTED_FIRM_DIR/vendor/lib64/"
         sudo cp -rfa "$(pwd)/LumiROM/Mods/tweaks/system/system/etc/init/"* "$EXTRACTED_FIRM_DIR/system/system/etc/init/"
         sudo cp -rfa "$(pwd)/LumiROM/Mods/wallpaper/system/system/priv-app/wallpaper-res/"* "$EXTRACTED_FIRM_DIR/system/system/priv-app/wallpaper-res/"
+        sudo cp -rfa "$(pwd)/LumiROM/Mods/Camera/"* "$EXTRACTED_FIRM_DIR/system/system/priv-app/"*
         echo " - Mods added"
     else
         echo "The use of mods for this build have been disabled by the user"
