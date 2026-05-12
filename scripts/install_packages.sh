37725fcca02ad6e0434f6edc8ba25c4c555ab94d
Luminous | 418 <jgutalg2006@outlook.es>
2026-05-12 12:07:21 +0000

    Add a fix for build_local

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -5,8 +5,8 @@
 sudo apt install -y -q p7zip-full android-sdk-libsparse-utils python3 python3-pip zipalign default-jre openjdk-17-jdk brotli e2fsprogs zstd aria2 unzip tar lz4 tree git-core gnupg flex bison build-essential zip curl zlib1g-dev libncurses5-dev libssl-dev libelf-dev libxml2-utils xsltproc unzip fontconfig python3 python3-protobuf
 
 # Installing Python packages.
-pip3 install liblp tgcrypto pyrogram
-pip3 install git+https://github.com/martinetd/samloader.git
+pip3 install liblp tgcrypto pyrogram --break-system-packages
+pip3 install git+https://github.com/martinetd/samloader.git --break-system-packages
 
 # Cleanup.
 sudo apt clean
