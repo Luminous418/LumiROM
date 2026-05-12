ebb760ee7cb4f4a27857335c51a0c8174fe08f2d
Luminous | 418 <jgutalg2006@outlook.es>
2026-05-12 15:52:14 +0000

    Add libprotobuf32t64 to packages needed

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -2,7 +2,7 @@
 
 # Linux setup.
 # Installing necessary packages.
-sudo apt install -y -q p7zip-full android-sdk-libsparse-utils python3 python3-pip zipalign default-jre openjdk-17-jdk brotli e2fsprogs zstd aria2 unzip tar lz4 tree git-core gnupg flex bison build-essential zip curl zlib1g-dev libncurses5-dev libssl-dev libelf-dev libxml2-utils xsltproc unzip fontconfig python3 python3-protobuf
+sudo apt install -y -q p7zip-full android-sdk-libsparse-utils python3 python3-pip zipalign default-jre openjdk-17-jdk brotli e2fsprogs zstd aria2 unzip tar lz4 tree git-core gnupg flex bison build-essential zip curl zlib1g-dev libncurses5-dev libssl-dev libelf-dev libxml2-utils xsltproc unzip fontconfig python3 python3-protobuf libprotobuf32t64
 
 # Installing Python packages.
 pip3 install liblp tgcrypto pyrogram --break-system-packages
